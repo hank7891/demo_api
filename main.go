de93192572289ee1cf73ad7b81dd472d3a2b3f02
Hank <hank7891@gmail.com>
2023-08-18 18:12:29 +0800

    feat: 變數宣告練習

--- main.go
+++ main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	variabletest()
+}
+
+// 變數練習
+func variabletest() {
+	// 同類型變數一起宣告
+	var a, b int = 1, 2
+	var c, python, java = true, false, "no!"
+	fmt.Println(a, b, c, python, java)
+
+	// 短宣告
+	a1, b1 := 1, 2
+	c1, python1, java1 := true, false, "no!"
+	fmt.Println(a1, b1, c1, python1, java1)
+
+	// var 混合宣告
+	var (
+		a2      = 1
+		b2      = 2
+		c2      = true
+		python2 = false
+		java2   = "no!"
+	)
+	fmt.Println(a2, b2, c2, python2, java2)
 }
